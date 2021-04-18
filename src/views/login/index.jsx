909108ba329a7f40efa123d8670297098f615ee4
liu_qihao <2893535402@qq.com>
2021-04-18 21:20:39 +0800

    home - 2021.4.18

--- src/views/login/index.jsx
+++ src/views/login/index.jsx
@@ -6,19 +6,74 @@ import { loginApi } from '@/api'
 
 export default class Login extends PureComponent {
   state = {
-    name: '',
-    pwd: ''
+    checked: false,
+    params: {
+      name: '',
+      pwd: ''
+    }
   }
 
   changeFormData = (label) => {
     return (event) => {
       this.setState(
-        { [label]: event.target.value },
+        ({ params }) => ({
+          params: {
+            ...params,
+            [label]: event.target.value
+          }
+        }),
+        () => {
+          console.log(this.state.params)
+        }
+      )
+    }
+  }
+
+  changeState = (label) => {
+    return (event) => {
+      this.setState(
+        () => ({
+          [label]: event.target.value || event.target.checked
+        }),
         () => {
           console.log(this.state)
         }
       )
     }
+  }
+
+  login = async () => {
+    const res = await loginApi.login(this.state)
+    if (res.data.success) {
+      window.sessionStorage.userInfo.set(res.data.data)
+      if (this.checked) {
+        window.localStorage.loginName = this.params.name
+      } else {
+        window.localStorage.loginName = ''
+      }
+      if (res.data.data.passwordExpired) {
+        message.warning('Your password has expired, please change it')
+      }
+      if (res.data.data.firstLogin) {
+        message.warning('Please change your password when you log in for the first time')
+      }
+      if (res.data.data.firstLogin || res.data.data.passwordExpired) {
+        this.$router.push({ name: 'firstLogin' })
+      } else {
+        this.$router.push({ name: 'siteSelect' })
+      }
+    }
+  }
+
+  // 
+  getSnapshotBeforeUpdate() {
+    console.log(1111)
+    return 'liu_qihao'
+  }
+  componentDidUpdate(prevProps, prevStata, snapshot) {
+    console.log(prevProps);
+    console.log(prevStata);
+    console.log(snapshot);
   }
 
   render() {
@@ -41,7 +96,7 @@ export default class Login extends PureComponent {
               </Form.Item>
               <Button type="primary" onClick="login">Log in</Button>
               <div className="remember">
-                <Checkbox v-model="checked">Remember me</Checkbox>
+                <Checkbox onChange={this.changeState('checked')}>Remember me</Checkbox>
                 <router-link to="/forgetPassword">Forgot password？</router-link>
               </div>
             </Form>
