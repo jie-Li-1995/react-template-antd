e3d882782f7819b1af22f52f7dc0f19a8c970b83
jie-Li-1995 <2893535402@qq.com>
2021-04-28 09:55:58 +0800

    change
    change

--- src/views/test.jsx
+++ src/views/test.jsx
@@ -1,27 +1,27 @@
-import React, { Component } from "react";
-import { Button, Input } from "antd";
-import { connect } from "react-redux";
-import { increment, decrement } from "@/store/modules/counter/actions";
+import React, { Component } from 'react'
+import { Button, Input } from 'antd'
+import { connect } from 'react-redux'
+import { increment, decrement } from '@/store/modules/counter/actions'
 
 @connect((state) => ({ counter: state.counter }), { increment, decrement })
 class Test extends Component {
   state = {
-    name: "test",
-  };
-
-  componentDidMount() {
-    console.log(this.state);
-    console.log(this.props);
+    name: 'test'
   }
 
-  getSnapshotBeforeUpdate(prevProps, prevState) {
+  componentDidMount () {
+    console.log(this.state)
+    console.log(this.props)
+  }
+
+  getSnapshotBeforeUpdate (prevProps, prevState) {
     // 两者的参数是不相同的，
     // 而getDerivedStateFromProps是一个静态函数，
     // 也就是这个函数不能通过this访问到class的属性，也并不推荐直接访问属性。
     // 而是应该通过参数提供的nextProps以及prevState来进行判断，根据新传入的props来映射到state。
   }
 
-  static getDerivedStateFromProps(nextProps, prevState) {
+  static getDerivedStateFromProps (nextProps, prevState) {
     // 两者的参数是不相同的，
     // 而getDerivedStateFromProps是一个静态函数，
     // 也就是这个函数不能通过this访问到class的属性，也并不推荐直接访问属性。
@@ -29,14 +29,14 @@ class Test extends Component {
   }
 
   add = () => {
-    let Number = this.refs.MySelect.state.value * 1;
-    this.props.increment(Number);
-  };
+    let Number = this.refs.MySelect.state.value * 1
+    this.props.increment(Number)
+  }
 
   cut = () => {
-    let Number = this.refs.MySelect.state.value * 1;
-    this.props.decrement(Number);
-  };
+    let Number = this.refs.MySelect.state.value * 1
+    this.props.decrement(Number)
+  }
 
   changeFormData = (label) => {
     return (event) => {
@@ -44,30 +44,30 @@ class Test extends Component {
         ({ params }) => ({
           params: {
             ...params,
-            [label]: event.target.value,
-          },
+            [label]: event.target.value
+          }
         }),
         () => {
-          console.log(this.state.params);
+          console.log(this.state.params)
         }
-      );
-    };
-  };
+      )
+    }
+  }
 
   changeState = (label) => {
     return (event) => {
       this.setState(
         () => ({
-          [label]: event.target.value || event.target.checked,
+          [label]: event.target.value || event.target.checked
         }),
         () => {
-          console.log(this.state);
+          console.log(this.state)
         }
-      );
-    };
-  };
+      )
+    }
+  }
 
-  render() {
+  render () {
     return (
       <div>
         <Input defaultValue={1} ref="MySelect" />
@@ -75,7 +75,7 @@ class Test extends Component {
         <Button onClick={this.add}>+</Button>
         <Button onClick={this.cut}>—</Button>
       </div>
-    );
+    )
   }
 }
 
