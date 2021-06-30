261a9728190f530ba743d80cc16c5f6a2afefa3e
jie-Li-1995 <2893535402@qq.com>
2021-06-30 10:06:39 +0800

    hook

--- src/views/content/createContent/index.jsx
+++ src/views/content/createContent/index.jsx
@@ -1,9 +1,13 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { memo, useState, useEffect, useRef, useMemo, useCallback } from 'react'
 import { Input, Button } from 'antd'
 
-export default function CreateContent() {
+export default function CreateContent () {
+  const [testName, setTestName] = useState('test')
   const [name, setName] = useState('小李')
-  const inputRef = useRef()
+  const inputRef = useRef(null)
+
+  const [val1, setVal1] = useState('')
+  const [val2, setVal2] = useState('')
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -16,8 +20,15 @@ export default function CreateContent() {
     }
   }, [name]) // 数组表示 effect 依赖的变量，只有当这个变量发生改变之后才会重新执行 efffect 函数
 
+  const onChange1 = useCallback(evt => {
+    setVal1(evt.target.value)
+  }, [])
+
+  const onChange2 = useCallback(evt => {
+    setVal2(evt.target.value)
+  }, [])
+
   const changeName = (val) => {
-    console.log(1111)
     setName(val)
     inputRef.current.focus()
   }
@@ -26,13 +37,31 @@ export default function CreateContent() {
     changeName(e.target.value)
   }
 
+  const computedName = useMemo(
+    () => {
+      console.log('computedName')
+      return testName + 'by liu_qihao'
+    },
+    [testName]
+  )
+
   return (
     <div>
+      <h1>{computedName}</h1>
       <Input value={name} ref={inputRef} onChange={inputName} />
       <Button onClick={() => setName('initData')} type="primary">
         initData
       </Button>
       <Button onClick={() => changeName('initData2')}>initData2</Button>
+      <br />
+      <Child val={val1} onChange={onChange1} />
+      <Child val={val2} onChange={onChange2} />
     </div>
   )
 }
+
+const Child = memo(function ({ val, onChange }) {
+  console.log('render...')
+
+  return <Input value={val} onChange={onChange} />
+})
